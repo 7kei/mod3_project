850f1dc9758c02aaa11a05410cc0c42ddea5112e
Keian Mar <150799485+7kei@users.noreply.github.com>
2023-10-21 00:20:57 +0800

    Add exchange rate display and conversion.

--- mod3_project/mod3_project.cpp
+++ mod3_project/mod3_project.cpp
@@ -1,34 +1,103 @@
 #include <iostream>
 #include <httplib.h>
 #include <nlohmann/json.hpp>
+#include <string>
 
 using json = nlohmann::json;
 int mainMenu();
+
+json apiGetter(std::string urlToGet)
+{
+    httplib::Client cli("openexchangerates.org");
+    cli.set_default_headers({ {"accept","application/json"} });
+    auto res = cli.Get(urlToGet);
+
+    if (res == nullptr)
+        return 1;
+
+    std::string bodyResponse = res->body;
+    return json::parse(bodyResponse);
+}
 
 int displayCurrencies()
 {
     // Clear terminal contents beforehand
     system("cls");
 
-    httplib::Client cli("openexchangerates.org");
-    cli.set_default_headers({{"accept","application/json"}});
-    auto res = cli.Get("/api/currencies.json?prettyprint=false&show_alternatives=false");
+    json bodyData = apiGetter("/api/currencies.json?prettyprint=false&show_alternatives=false");
 
-    if (res == nullptr)
-        return 1;
-    
-    std::string bodyResponse = res->body;
-    json bodyData = json::parse(bodyResponse);
-
-    printf("#----------------------------------------#\n");
+    std::cout << "#----------------------------------------#" << std::endl;
     for (auto& elem : bodyData.items()) {
-        std::string key = elem.key();
-        std::string value = elem.value();
-        printf("%s, %s\n", key.c_str(), value.c_str());
+        std::cout << elem.key() << ", " << elem.value() << std::endl;
     }
-    printf("#----------------------------------------#\n");
-    printf("# Press any key to return to menu.       #\n");
-    printf("#----------------------------------------#\n");
+    std::cout << "#----------------------------------------#\n"
+              << "# Press any key to return to menu.       #\n"
+              << "#----------------------------------------#"   << std::endl;
+    system("pause");
+    mainMenu();
+
+    return 0;
+}
+
+int displayExchangeRate()
+{
+    system("cls");
+
+    std::string apiKey = "";
+    json latestExchangeRateResp = apiGetter("/api/latest.json?app_id="+apiKey+"&show_alternative = false");
+
+    std::cout << "#----------------------------------------#\n"
+              << "# From base USD:                         #" << std::endl;
+    for (auto& elem : latestExchangeRateResp["rates"].items()) {
+        std::cout << elem.key() << ", " << elem.value() << std::endl;
+    }
+    std::cout << "#----------------------------------------#\n"
+              << "# Press any key to return to menu.       #\n"
+              << "#----------------------------------------#" << std::endl;
+    system("pause");
+    mainMenu();
+
+    return 0;
+}
+
+int convertCurrency()
+{
+    system("cls");
+
+    long double amount;
+    char fromCurrency[4]{};
+    char toCurrency[4]{};
+
+    long double toUsd;
+    long double toTargetCurrency;
+
+    std::cout << "#----------------------------------------#\n"
+              << "# Input your desired amount:             #\n"
+              << "#----------------------------------------#" << std::endl;
+
+    std::cin >> amount;
+
+    std::cout << "#----------------------------------------#\n"
+              << "# Input from currency                    #\n"
+              << "#----------------------------------------#" << std::endl;
+    std::cin >> fromCurrency;
+
+    std::cout << "#----------------------------------------#\n"
+              << "# Input to currency                      #\n"
+              << "#----------------------------------------#" << std::endl;
+    std::cin >> toCurrency;
+
+    std::string apiKey = "";
+    json latestExchangeRateResp = apiGetter("/api/latest.json?app_id=" + apiKey + "&show_alternative = false");
+
+    // convert to usd
+    toUsd = amount / (long double)latestExchangeRateResp["rates"][fromCurrency];
+    std::cout << amount << " / " << (long double)latestExchangeRateResp["rates"][fromCurrency] << " = " << toUsd << std::endl;
+    toTargetCurrency = toUsd * (long double)latestExchangeRateResp["rates"][toCurrency];
+    std::cout << toUsd << " * " << (long double)latestExchangeRateResp["rates"][toCurrency] << " = " << toTargetCurrency << std::endl;
+
+    std::cout << toTargetCurrency << std::endl;
+
     system("pause");
     mainMenu();
 
@@ -39,19 +108,25 @@ int mainMenu() {
     system("cls");
     unsigned char choice;
 
-    printf("#----------------------------------------#\n");
-    printf("# Welcome to the Billing System!         #\n");
-    printf("# Select from your functions from below: #\n");
-    printf("# 1. Display all supported currencies    #\n");
-    printf("# 2. Convert currencies                  #\n");
-    printf("#----------------------------------------#\n");
-    scanf_s("%c", &choice);
+    std::cout << "#----------------------------------------#\n"
+              << "# Welcome to the Billing System!         #\n"
+              << "# Select from your functions from below: #\n"
+              << "# 1. Display all supported currencies    #\n"
+              << "# 2. Display current exchange rates      #\n"
+              << "# 3. Convert currencies                  #\n"
+              << "#----------------------------------------#"   << std::endl;
+
+    std::cin >> choice;
 
     switch (choice) {
     case '1':
         return displayCurrencies();
         break;
     case '2':
+        return displayExchangeRate();
+        break;
+    case '3':
+        return convertCurrency();
         break;
     default:
         mainMenu();
@@ -66,7 +141,7 @@ int main()
     system("cls");
 
     // Menu
-    mainMenu();
+    return mainMenu();
         
 }
 
